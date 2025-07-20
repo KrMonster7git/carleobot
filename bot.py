2b6b3c73d60fcab4436ec5d07b577342bc92aad6
KrMonster7git <radion.kucherukm@gmail.com>
2025-07-20 23:39:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -55,7 +55,7 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 # Запуск
 import os
-app = ApplicationBuilder().token(os.environ["BOT_TOKEN]).build()
+app = ApplicationBuilder().token("8014079225:AAERc8oELijpmfiLswn-rUm1ovd3k3SaYWc").build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CallbackQueryHandler(button))
 
